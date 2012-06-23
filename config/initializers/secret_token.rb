d26b494587dc919064ee30164af721a76ab18def
Ryo NAKAMURA <r7kamura@gmail.com>
2012-06-23 13:19:33 +0900

    Initial commit
    using following command:
    $ rails new babel -T -d mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babel::Application.config.secret_token = 'e8b9ba2b730fe33203e166a4926cbeda845bd1bc8caf4bace1c278d93bdd39e03abde36ddae72f00bf44b2ab95918182d3fb5365e64731bd1ba3ac2584175658'
