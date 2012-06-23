2236aabbee80fce82677190c50b0ee55cd30597b
Ryo NAKAMURA <r7kamura@gmail.com>
2012-06-23 17:44:24 +0900

    Design show page

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -2,4 +2,17 @@ module EntriesHelper
   def gist_embed_tag(code)
     javascript_include_tag "#{code.url}.js"
   end
+
+  def label_class_name(str)
+    names = [
+      "",
+      "label-success",
+      "label-important",
+      "label-warning",
+      "label-important",
+      "label-info",
+      "label-inverse",
+    ]
+    names[str.to_i(36) % names.size]
+  end
 end
