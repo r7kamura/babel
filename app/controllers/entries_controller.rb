181fccc842fbb15bc478a0dd5130c61feeb4b72a
Ryo NAKAMURA <r7kamura@gmail.com>
2012-06-23 13:38:22 +0900

    Create EntriesController

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,29 @@
+class EntriesController < ApplicationController
+  before_filter :prepare_entry, :only => :show
+
+  def index
+    @entries = Entry.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    if @entry = Entry.create(params[:entry])
+      redirect_to @entry
+    else
+      redirect_to entries_path
+    end
+  end
+
+  private
+
+  def prepare_entry
+    @entry = Entry.find_by_id(params[:id]) or
+      redirect_to entries_path
+  end
+end
